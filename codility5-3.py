baac02e98d793de7d8a64f9f3ba86fa859e97dcf
minkyu2346 <kmk2346@naver.com>
2022-02-10 11:48:37 +0900

    genomicrangequery

---
+++ codility5-3.py
@@ -0,0 +1,26 @@
+def solution(S, P, Q):
+    result=[]
+    for i ,v in zip(P,Q):
+
+        new_list= S[i:v+1]
+        print(new_list)
+
+        if new_list.find("A")!=-1:
+
+            result.append(1)
+            continue
+
+        if new_list.find("C")!=-1:
+
+            result.append(2)
+            continue
+        if new_list.find("G")!=-1:
+
+            result.append(3)
+            continue
+        
+        else:
+
+            result.append(4)
+        
+    return result
